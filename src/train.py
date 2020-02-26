a4437403e7dd02b3f329a44f2b1c7e3638ac250e
Kolin Guo <kolguo@ucdavis.edu>
2020-02-26 06:54:45 -0800

    save status

---
+++ src/train.py
@@ -0,0 +1,20 @@
+import argparse
+import gym
+import gym_sokoban
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+def get_train_args():
+    train_args = argparse.ArgumentParser()
+
+    
+
+    return train_args.parse_args()
+
+def train(args):
+
+if __name__ == '__main__':
+    train_args = get_train_args()
+    train(train_args)
+
---
+++ src/utils/experience_replay.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+class ReplayMemory:
+    def __init__(self, args):
+
